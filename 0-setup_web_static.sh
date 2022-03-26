1d474ac7f9fbdc900ca89c0803085e57f1cd3e93
MikiyasA <mikiyasad86@gmail.com>
2022-03-26 09:30:08 -0700

    To fixt task 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
 apt-get -y update
@@ -23,17 +22,3 @@ chown -R ubuntu:ubuntu /data/
 sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
-=======
-!/usr/bin/env bash
-# sets up the web servers for the deployment of web_static
-
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
->>>>>>> bcbcb708acbe63176197926a88d965cedd8399e1
