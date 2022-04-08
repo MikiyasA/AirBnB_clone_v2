c6ae82f8e19e26ef6f43de8124597ff5e7f5691e
MikiyasA <mikiyasad86@gmail.com>
2022-04-08 10:39:39 -0700

    To commit some tasks

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,5 +14,9 @@ app = Flask(__name__)
 def display():    
   return "Hello HBNB"
 
+@app.route('/hbnb/', strict_slashes=False)
+def hbnb():
+  return "HBNB"
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -14,5 +14,14 @@ app = Flask(__name__)
 def display():    
   return "Hello HBNB"
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+  return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+  return "C {}".format(text.replace('_', ' '))
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+  app.run(host='0.0.0.0', port=5000)
+
